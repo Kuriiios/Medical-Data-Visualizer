965b845420d83541ca2838c1bc320ef18d3f9891
Cyril Leconte <cyril.leconte07@gmail.com>
2024-08-03 22:30:40 +0200

    created draw_cat_plot function step 4 through 9

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,36 +4,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / (df['height']/100)**2) > 25
+df['overweight'] = df['overweight'].astype(int)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].replace([1, 2, 3], [0, 1, 1])
+df['gluc'] = df['gluc'].replace([1, 2, 3], [0, 1, 1])
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = df.melt(id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'], var_name='variable', value_name='value' )
+
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
 
-
+    sns.set_theme(style='ticks')
 
     # 8
-    fig = None
+    graph = sns.catplot(x='variable', y='total', hue='value', kind='bar', col='cardio', data=df_cat)
+    graph.set_axis_labels('Variable', 'Total')
+    graph.set_titles('Cardio: {col_name}')
 
 
     # 9
-    fig.savefig('catplot.png')
-    return fig
-
+    plt.savefig('catplot.png')
+    plt.show()
+    #return fig
+draw_cat_plot()
 
 # 10
 def draw_heat_map():
